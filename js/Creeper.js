00c3e6471ecbec73eab045931bc572723c44adf4
Eason Kang <eason.kang@autodesk.com>
2019-04-16 16:49:39 +0800

    Minecraft Creeper empty project

---
+++ js/Creeper.js
@@ -0,0 +1,34 @@
+/**
+ * Minecraft Creeper model
+ * Ref: https://ithelp.ithome.com.tw/articles/10200287
+ */
+
+class Creeper extends THREE.Group {
+  constructor() {
+    super();
+
+    this.init();
+  }
+
+  init() {
+    // Geometries for head, body and foot
+    
+    // Skin Color
+    
+
+    // head
+    
+
+    // body
+    
+
+    // 4 feet
+    
+
+    // Combine 4 feet as a group
+    
+
+    // Put head, body and feet into a single group
+    
+  }
+}
--- js/index.js
+++ js/index.js
@@ -5,21 +5,80 @@
     const scene = new THREE.Scene();
 
     // Add actors of this scene
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshPhongMaterial( { color: 0x0000ff } );
-    const cube = new THREE.Mesh( geometry, material );
-    cube.position.set( 0, 0, 0 );
-    scene.add( cube );
+
+    // Axes helper
+    let axes = new THREE.AxesHelper( 20 )
+    scene.add( axes );// Put your three.js codes below
+    (function() {
+    
+        // Prepare stage scene
+        const scene = new THREE.Scene();
+    
+        // Add actors of this scene
+    
+        // Axes helper
+        
+    
+        // Creeper
+        
+    
+        // Ground
+        
+      
+        // Add camera
+        
+      
+        // Set up stage light
+        
+      
+        // Set up renderer
+        const renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.setClearColor( 0xeeeeee, 1.0 );
+        renderer.shadowMap.enable = true;
+      
+        document.body.appendChild( renderer.domElement );
+      
+        // Start rendering.
+        function render() {
+          requestAnimationFrame( render );
+          renderer.render( scene, camera );
+        }
+      
+        render();
+    
+        window.addEventListener( 'resize', function() {
+          camera.aspect = window.innerWidth / window.innerHeight;
+    
+          camera.updateProjectionMatrix();
+    
+          renderer.setSize( window.innerWidth, window.innerHeight );
+        });
+    
+    })();
+
+    // Creeper
+    const creeper = new Creeper();
+    scene.add( creeper );
+
+    // Ground
+    const planeGeometry = new THREE.PlaneGeometry( 60, 60 );
+    const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    let plane = new THREE.Mesh( planeGeometry, planeMaterial );
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.set( 0, -7, 0 );
+    scene.add( plane );
   
     // Add camera
-    const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 100 );
-    camera.position.set( 10, 10, 10 );
+    const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.position.set( 30, 30, 30 );
     camera.lookAt( scene.position );
   
     // Set up stage light
-    const pointLight = new THREE.PointLight( 0xffffff );
-    pointLight.position.set( 10, 10, -10 );
-    scene.add( pointLight );
+    const spotLight = new THREE.SpotLight(0xffffff)
+    spotLight.position.set( -10, 40, 30)
+    scene.add(spotLight)
   
     // Set up renderer
     const renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -30,15 +89,8 @@
   
     document.body.appendChild( renderer.domElement );
   
-    //Make the cube rotate
-    function animate() {
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-    }
-  
     // Start rendering.
     function render() {
-      animate();
       requestAnimationFrame( render );
       renderer.render( scene, camera );
     }
@@ -47,7 +99,7 @@
 
     window.addEventListener( 'resize', function() {
       camera.aspect = window.innerWidth / window.innerHeight;
-      
+
       camera.updateProjectionMatrix();
 
       renderer.setSize( window.innerWidth, window.innerHeight );
