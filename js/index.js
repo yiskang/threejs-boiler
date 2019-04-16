a48cd6a883dd11882dd1b82d22668899f4f85aac
Eason Kang <eason.kang@autodesk.com>
2019-04-16 20:05:56 +0800

    Orbit control

--- js/index.js
+++ js/index.js
@@ -26,6 +26,11 @@
     const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
     camera.position.set( 30, 30, 30 );
     camera.lookAt( scene.position );
+
+    // Add OrbitControls
+    const navTool = new THREE.OrbitControls( camera );
+    navTool.enableDamping = true;
+    navTool.dampingFactor = 0.25;
   
     // Set up stage light
     const spotLight = new THREE.SpotLight( 0xffffff );
@@ -43,6 +48,8 @@
   
     // Start rendering.
     function render() {
+      navTool.update();
+      
       requestAnimationFrame( render );
       renderer.render( scene, camera );
     }
