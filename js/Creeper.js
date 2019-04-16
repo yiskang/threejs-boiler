bf3fbec1ee5f6259d5bca08b51084933061856f9
Eason Kang <eason.kang@autodesk.com>
2019-04-16 19:02:18 +0800

    Basic Creeper

--- js/Creeper.js
+++ js/Creeper.js
@@ -12,23 +12,41 @@ class Creeper extends THREE.Group {
 
   init() {
     // Geometries for head, body and foot
-    
+    const headGeo = new THREE.BoxGeometry( 4, 4, 4 );
+    const bodyGeo = new THREE.BoxGeometry( 4, 8, 2 );
+    const footGeo = new THREE.BoxGeometry( 2, 3, 2 );
+
     // Skin Color
-    
+    const creeperMat = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
 
     // head
-    
+    this.head = new THREE.Mesh( headGeo, creeperMat );
+    this.head.position.set( 0, 6, 0 );
 
     // body
-    
+    this.body = new THREE.Mesh( bodyGeo, creeperMat );
+    this.body.position.set( 0, 0, 0 );
 
     // 4 feet
-    
+    this.foot1 = new THREE.Mesh( footGeo, creeperMat );
+    this.foot1.position.set( -1, -5.5, 2 );
+    this.foot2 = this.foot1.clone();
+    this.foot2.position.set( -1, -5.5, -2 );
+    this.foot3 = this.foot1.clone();
+    this.foot3.position.set( 1, -5.5, 2 );
+    this.foot4 = this.foot1.clone();
+    this.foot4.position.set( 1, -5.5, -2 );
 
     // Combine 4 feet as a group
-    
+    this.feet = new THREE.Group();
+    this.feet.add( this.foot1 );
+    this.feet.add( this.foot2 );
+    this.feet.add( this.foot3 );
+    this.feet.add( this.foot4 );
 
     // Put head, body and feet into a single group
-    
+    this.add( this.head );
+    this.add( this.body );
+    this.add( this.feet );
   }
 }
--- js/index.js
+++ js/index.js
@@ -7,56 +7,8 @@
     // Add actors of this scene
 
     // Axes helper
-    let axes = new THREE.AxesHelper( 20 )
-    scene.add( axes );// Put your three.js codes below
-    (function() {
-    
-        // Prepare stage scene
-        const scene = new THREE.Scene();
-    
-        // Add actors of this scene
-    
-        // Axes helper
-        
-    
-        // Creeper
-        
-    
-        // Ground
-        
-      
-        // Add camera
-        
-      
-        // Set up stage light
-        
-      
-        // Set up renderer
-        const renderer = new THREE.WebGLRenderer( { antialias: true } );
-        renderer.setPixelRatio( window.devicePixelRatio );
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.setClearColor( 0xeeeeee, 1.0 );
-        renderer.shadowMap.enable = true;
-      
-        document.body.appendChild( renderer.domElement );
-      
-        // Start rendering.
-        function render() {
-          requestAnimationFrame( render );
-          renderer.render( scene, camera );
-        }
-      
-        render();
-    
-        window.addEventListener( 'resize', function() {
-          camera.aspect = window.innerWidth / window.innerHeight;
-    
-          camera.updateProjectionMatrix();
-    
-          renderer.setSize( window.innerWidth, window.innerHeight );
-        });
-    
-    })();
+    const axes = new THREE.AxesHelper( 20 );
+    scene.add( axes );
 
     // Creeper
     const creeper = new Creeper();
@@ -65,7 +17,7 @@
     // Ground
     const planeGeometry = new THREE.PlaneGeometry( 60, 60 );
     const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
-    let plane = new THREE.Mesh( planeGeometry, planeMaterial );
+    const plane = new THREE.Mesh( planeGeometry, planeMaterial );
     plane.rotation.x = -0.5 * Math.PI;
     plane.position.set( 0, -7, 0 );
     scene.add( plane );
@@ -76,9 +28,9 @@
     camera.lookAt( scene.position );
   
     // Set up stage light
-    const spotLight = new THREE.SpotLight(0xffffff)
-    spotLight.position.set( -10, 40, 30)
-    scene.add(spotLight)
+    const spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.position.set( -10, 40, 30 );
+    scene.add( spotLight );
   
     // Set up renderer
     const renderer = new THREE.WebGLRenderer( { antialias: true } );
